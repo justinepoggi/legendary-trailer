d4544f63fdfe7ff0513fe82bee21977f0d3b4787
justinepoggi <justpoggi@gmail.com>
2021-01-09 16:48:05 +0100

    Add generated reactive Spring Boot project

---
+++ src/main/java/com/legendarytrailer/crazywallet/CrazyWalletApplication.java
@@ -0,0 +1,13 @@
+package com.legendarytrailer.crazywallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legendarytrailer/crazywallet/CrazyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legendarytrailer.crazywallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
